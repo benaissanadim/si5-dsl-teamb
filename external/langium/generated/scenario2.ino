c59b257fdd4e171268086c4bf0759261d9b5bfe3
ImeneYAHIAOUI <90778346+ImeneYAHIAOUI@users.noreply.github.com>
2023-11-27 11:13:41 +0100

    change embedded to external

--- external/langium/generated/scenario2.ino
+++ external/langium/generated/scenario2.ino
@@ -29,62 +29,56 @@ long button2LastDebounceTime = 0;
 				case OneButtonPressed:
 					digitalWrite(11,HIGH);
 					digitalWrite(12,LOW);
-		 			button1BounceGuard = millis() - button1LastDebounceTime > debounce;
+					button1BounceGuard = millis() - button1LastDebounceTime > debounce;
 					button2BounceGuard = millis() - button2LastDebounceTime > debounce;
+					
 					if ( ( ( digitalRead(button1.inputPin) == HIGH && button1BounceGuard ) && ( digitalRead(button2.inputPin) == HIGH && button2BounceGuard ) ) ) {
-					button1LastDebounceTime = millis();
-					button2LastDebounceTime = millis();
-					currentState = TwoButtonsPressed;
+						button1LastDebounceTime = millis();
+						button2LastDebounceTime = millis();
+						currentState = TwoButtonsPressed;
 					}
-		
-		 			button1BounceGuard = millis() - button1LastDebounceTime > debounce;
-					button2BounceGuard = millis() - button2LastDebounceTime > debounce;
 					if ( ( ( digitalRead(button1.inputPin) == LOW && button1BounceGuard ) && ( digitalRead(button2.inputPin) == LOW && button2BounceGuard ) ) ) {
-					button1LastDebounceTime = millis();
-					button2LastDebounceTime = millis();
-					currentState = off;
+						button1LastDebounceTime = millis();
+						button2LastDebounceTime = millis();
+						currentState = off;
 					}
-		
+					
 				break;
 				case TwoButtonsPressed:
 					digitalWrite(11,HIGH);
 					digitalWrite(12,HIGH);
-		 			button1BounceGuard = millis() - button1LastDebounceTime > debounce;
+					button1BounceGuard = millis() - button1LastDebounceTime > debounce;
 					button2BounceGuard = millis() - button2LastDebounceTime > debounce;
+					
 					if ( ( ( digitalRead(button1.inputPin) == LOW && button1BounceGuard ) ^ ( digitalRead(button2.inputPin) == LOW && button2BounceGuard ) ) ) {
-					button1LastDebounceTime = millis();
-					button2LastDebounceTime = millis();
-					currentState = OneButtonPressed;
+						button1LastDebounceTime = millis();
+						button2LastDebounceTime = millis();
+						currentState = OneButtonPressed;
 					}
-		
-		 			button1BounceGuard = millis() - button1LastDebounceTime > debounce;
-					button2BounceGuard = millis() - button2LastDebounceTime > debounce;
 					if ( ( ( digitalRead(button1.inputPin) == LOW && button1BounceGuard ) && ( digitalRead(button2.inputPin) == LOW && button2BounceGuard ) ) ) {
-					button1LastDebounceTime = millis();
-					button2LastDebounceTime = millis();
-					currentState = off;
+						button1LastDebounceTime = millis();
+						button2LastDebounceTime = millis();
+						currentState = off;
 					}
-		
+					
 				break;
 				case off:
 					digitalWrite(11,LOW);
 					digitalWrite(12,LOW);
-		 			button1BounceGuard = millis() - button1LastDebounceTime > debounce;
+					button1BounceGuard = millis() - button1LastDebounceTime > debounce;
 					button2BounceGuard = millis() - button2LastDebounceTime > debounce;
+					
 					if ( ( ( digitalRead(button1.inputPin) == HIGH && button1BounceGuard ) && ( digitalRead(button2.inputPin) == HIGH && button2BounceGuard ) ) ) {
-					button1LastDebounceTime = millis();
-					button2LastDebounceTime = millis();
-					currentState = TwoButtonsPressed;
+						button1LastDebounceTime = millis();
+						button2LastDebounceTime = millis();
+						currentState = TwoButtonsPressed;
 					}
-		
-		 			button1BounceGuard = millis() - button1LastDebounceTime > debounce;
-					button2BounceGuard = millis() - button2LastDebounceTime > debounce;
 					if ( ( ( digitalRead(button1.inputPin) == HIGH && button1BounceGuard ) ^ ( digitalRead(button2.inputPin) == HIGH && button2BounceGuard ) ) ) {
-					button1LastDebounceTime = millis();
-					button2LastDebounceTime = millis();
-					currentState = OneButtonPressed;
+						button1LastDebounceTime = millis();
+						button2LastDebounceTime = millis();
+						currentState = OneButtonPressed;
 					}
-		
+					
 				break;
 		}
 	}
