355ac389bfc30aedb621edd114d884f09d3d349c
BEN AISSA Nadim <80357350+benaissanadim@users.noreply.github.com>
2023-11-28 21:51:39 +0100

    edit scenario

--- external/langium/generated/scenario1.ino
+++ external/langium/generated/scenario1.ino
@@ -15,12 +15,14 @@ long buttonLastDebounceTime = 0;
 	void setup(){
 		pinMode(9, INPUT); // button [Sensor]
 		pinMode(11, OUTPUT); // led [Actuator]
+		pinMode(12, OUTPUT); // buzzer [Actuator]
 	}
 	void loop() {
 			switch(currentState){
 
 				case on:
 					digitalWrite(11,HIGH);
+					digitalWrite(12,HIGH);
 					buttonBounceGuard = millis() - buttonLastDebounceTime > debounce;
 					if ( digitalRead(9) == LOW && buttonBounceGuard ) {
 						buttonLastDebounceTime = millis();
@@ -30,6 +32,7 @@ long buttonLastDebounceTime = 0;
 				  break;
 				case off:
 					digitalWrite(11,LOW);
+					digitalWrite(12,LOW);
 					buttonBounceGuard = millis() - buttonLastDebounceTime > debounce;
 					if ( digitalRead(9) == HIGH && buttonBounceGuard ) {
 						buttonLastDebounceTime = millis();
