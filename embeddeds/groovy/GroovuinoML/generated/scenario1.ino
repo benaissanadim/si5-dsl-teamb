bd8b40919f0f016b972d2c80e85cc39bef6fdd4f
BEN AISSA Nadim <80357350+benaissanadim@users.noreply.github.com>
2023-11-28 22:06:32 +0100

    fix

--- embeddeds/groovy/GroovuinoML/generated/scenario1.ino
+++ embeddeds/groovy/GroovuinoML/generated/scenario1.ino
@@ -12,20 +12,23 @@ long buttonLastDebounceTime = 0;
 void setup(){
   pinMode(9, INPUT);  // button [Sensor]
   pinMode(11, OUTPUT); // led [Actuator]
+  pinMode(12, OUTPUT); // buzzer [Actuator]
 }
 
 void loop() {
 	switch(currentState){
 		case on:
 			digitalWrite(11,HIGH);
+			digitalWrite(12,HIGH);
 			buttonBounceGuard = millis() - buttonLastDebounceTime > debounce;
-			if( digitalRead(9) == HIGH && buttonBounceGuard) {
+			if( digitalRead(9) == LOW && buttonBounceGuard) {
 				buttonLastDebounceTime = millis();
 				currentState = off;
 			}
 		break;
 		case off:
 			digitalWrite(11,LOW);
+			digitalWrite(12,LOW);
 			buttonBounceGuard = millis() - buttonLastDebounceTime > debounce;
 			if( digitalRead(9) == HIGH && buttonBounceGuard) {
 				buttonLastDebounceTime = millis();
---
+++ embeddeds/groovy/GroovuinoML/scenarios/Switch1.groovy
@@ -0,0 +1,13 @@
+sensor "button" onPin 9
+actuator "led" pin 11
+actuator "buzzer" pin 12
+
+state "on" means "led" becomes "high" and "buzzer" becomes "high"
+state "off" means "led" becomes "low" and "buzzer" becomes "low"
+
+initial "off"
+
+from "off" to "on" when "button" becomes "high"
+from "on" to "off" when "button" becomes "low"
+
+export "Switch!"
