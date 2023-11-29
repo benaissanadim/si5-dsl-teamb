5202efdd4817e98344c4c599a5f808bf3b0aa804
Sourour GAZZEH <79751594+sourour9@users.noreply.github.com>
2023-11-29 13:56:38 +0100

    fix

--- embeddeds/kernels/jvm/src/main/java/io/github/mosser/arduinoml/kernel/behavioral/Condition.java
+++
@@ -1,7 +0,0 @@
-package io.github.mosser.arduinoml.kernel.behavioral;
-
-import io.github.mosser.arduinoml.kernel.generator.Visitable;
-
-public interface Condition extends Visitable {
-    public String getCondition();
-}
