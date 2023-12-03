6abb70806aefa8d9d24c1d1cbb6af7144237c3e2
badr-ach <alachkarbadr@gmail.com>
2023-12-03 20:14:02 +0100

    add : error scenario state in kernel

--- embeddeds/kernels/jvm/src/main/java/io/github/mosser/arduinoml/kernel/behavioral/State.java
+++ embeddeds/kernels/jvm/src/main/java/io/github/mosser/arduinoml/kernel/behavioral/State.java
@@ -10,9 +10,15 @@ import java.util.List;
 public class State implements NamedElement, Visitable {
 
 	private String name;
-	private List<Action> actions = new ArrayList<>();
 	private List<ConditionalTransition> transitions = new ArrayList<>();
+	private List<Action> actions = new ArrayList<>();
+	public List<Action> getActions() {
+		return actions;
+	}
 
+	public void setActions(List<Action> actions) {
+		this.actions = actions;
+	}
 	@Override
 	public String getName() {
 		return name;
@@ -23,13 +29,7 @@ public class State implements NamedElement, Visitable {
 		this.name = name;
 	}
 
-	public List<Action> getActions() {
-		return actions;
-	}
 
-	public void setActions(List<Action> actions) {
-		this.actions = actions;
-	}
 	public void addTransition(ConditionalTransition transition) {
 		this.transitions.add(transition);
 	}
